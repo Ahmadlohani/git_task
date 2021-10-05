c87f66a266a6439d95c076ef27b67ccef3503a70
Ahmadlohani <ah203076@gmail.com>
2021-10-05 16:52:01 +0500

    new changes

--- action.php
+++ action.php
@@ -4,4 +4,5 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $_SESSION["name"] = $_POST["name"];
     $_SESSION["username"] = $_POST["username"];
     $_SESSION["email"] = $_POST["email"];
+    header("location: index.php");
 }
